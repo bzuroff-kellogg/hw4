cfeff024cafa7eee1aeb6365ce5b04d4b24c138a
bzuroff-kellogg <brittany.zuroff@kellogg.northwestern.edu>
2026-03-15 20:20:26 +0000

    first half of assignment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,30 @@
 class SessionsController < ApplicationController
   def new
+    
   end
 
   def create
+     @user = User.find_by({ "email" => params["email"] })
+  if @user
+ if BCrypt::Password.new(@user["password"]) == params["password"]
+ session["user_id"] = @user["id"]
+  
+      # login the user
+      flash["notice"] = "You've logged in."
+      redirect_to "/places"
+    else
+      flash["notice"] = "Unsuccessful login."
+      redirect_to "/sessions/new"
+    end
+  else
+    flash["notice"] = "Unsuccessful login."
+    redirect_to "/sessions/new"
+  end
   end
 
   def destroy
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
   
